44dce04df169cb929f7dc9594381869c88443741
Service Web <sw@gssmail.com>
2024-03-12 09:01:24 -0500

    Commit changes in C:\Websites\Git\emarlow\4482\GCG_4482_FactoryWiz.sql

---
+++ GCG_4482_FactoryWiz.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_4482_FactoryWiz"(
+ "Job" CHAR(6),
+ "Suffix" CHAR(3),
+ "Seq" CHAR(6),
+ "Machine" CHAR(4),
+ "Serial" BIGINT,
+ "Employee" CHAR(20),
+ "Employee_Start" DATETIME,
+ "Employee_Stop" DATETIME);
